8e470397915de4e038a5f4b1bb7f328f27fb5764
charmaine-dlamini-18 <222056401@mycput.ac.za>
2026-03-16 22:43:24 +0200

    Booking with Factory class

--- src/main/java/za/ac/cput/factory/BookingFactory.java
+++ src/main/java/za/ac/cput/factory/BookingFactory.java
@@ -1,4 +1,41 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Booking;
+import za.ac.cput.domain.Payment;
+import za.ac.cput.util.Helper;
+
 public class BookingFactory {
-}
+        public static Booking createBooking(String bookingId, String sessionId,
+                                            String sessionTime, String sessionType,
+                                            String duration, String studentNumber,
+                                            String subjectCode, String tutorId,
+                                            Payment payment){
+
+            if(Helper.isNullOrEmpty(bookingId)
+                    || Helper.isNullOrEmpty(sessionId)
+                    || Helper.isNullOrEmpty(sessionTime)
+                    || Helper.isNullOrEmpty(sessionType)
+                    || Helper.isNullOrEmpty(duration)
+                    || Helper.isNullOrEmpty(studentNumber)
+                    || Helper.isNullOrEmpty(subjectCode)
+                    || Helper.isNullOrEmpty(tutorId)
+                    || Helper.isNull(payment)
+            ){
+
+                return null;
+            }
+            return new Booking.Builder()
+                    .setBookingId(bookingId)
+                    .setSessionId(sessionId)
+                    .setSessionTime(sessionTime)
+                    .setSessionType(sessionType)
+                    .setDuration(duration)
+                    .setStudentNumber(studentNumber)
+                    .setSubjectCode(subjectCode)
+                    .setTutorId(tutorId)
+                    .setPayment(payment)
+                    .build();
+
+        }
+    }
+
