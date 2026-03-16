c86dcfd3c5759d6216d3756d77289997b303d5df
CocoMacNugget <safiyamelmi19@gmail.com>
2026-03-16 16:40:49 +0200

    Payment class with builder pattern

--- src/main/java/za/ac/cput/domain/Payment.java
+++ src/main/java/za/ac/cput/domain/Payment.java
@@ -1,5 +1,81 @@
 package za.ac.cput.domain;
 
-public class Payment {
+/*
+Payment.java
+Payment domain class
+Author: Safiya Elmi
+(240500598)
+Date: 16 March 2026
+*/
 
+public class Payment {
+    private final String paymentRef;
+    private final double amount;
+    private final String paymentDate;
+    private final String paymentMethod;
+    private final String status;
+
+    private Payment(Builder builder) {
+        this.paymentRef = builder.paymentRef;
+        this.amount = builder.amount;
+        this.paymentDate = builder.paymentDate;
+        this.paymentMethod = builder.paymentMethod;
+        this.status = builder.status;
+    }
+
+    public String getPaymentRef() {
+        return paymentRef;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public static class Builder {
+        private final String paymentRef;
+        private double amount;
+        private String paymentDate;
+        private String paymentMethod;
+        private String status;
+
+        public Builder(String paymentRef) {  // required field
+            this.paymentRef = paymentRef;
+        }
+
+        public Builder amount(double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder paymentDate(String paymentDate) {
+            this.paymentDate = paymentDate;
+            return this;
+        }
+
+        public Builder paymentMethod(String paymentMethod) {
+            this.paymentMethod = paymentMethod;
+            return this;
+        }
+
+        public Builder status(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Payment build() {
+            return new Payment(this);
+        }
+    }
 }
