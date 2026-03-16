a660aa966c0e8b9719f2796222009b0da59ec08c
Wendy-bayise <222828978@mycput.ac.za>
2026-03-17 00:42:49 +0200

    Subject with Factory class

--- src/main/java/za/ac/cput/factory/SubjectFactory.java
+++ src/main/java/za/ac/cput/factory/SubjectFactory.java
@@ -1,4 +1,27 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Subject;
+import za.ac.cput.util.Helper;
+
 public class SubjectFactory {
+
+    public static Subject createSubject(String subjectCode,
+                                        String subjectName, String subjectDescription,
+                                        String gradeLevel) {
+
+        if (Helper.isNullOrEmpty(subjectCode)
+                || Helper.isNullOrEmpty(subjectName)
+                || Helper.isNullOrEmpty(subjectDescription)
+                || Helper.isNullOrEmpty(gradeLevel))
+        {
+            return null;
+        }
+        return new Subject.Builder()
+                .setSubjectCode(subjectCode)
+                .setSubjectName(subjectName)
+                .setSubjectDescription(subjectDescription)
+                .setGradeLevel(gradeLevel)
+                .build();
+
+    }
 }
